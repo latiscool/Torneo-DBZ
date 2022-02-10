020eaf3230b0ab7a2abdcb40facf7f6c5fdc5621
latiscool <luistorresgomez.cl@gmail.com>
2022-02-10 03:45:01 -0300

    Update Consulta.js

--- assets/js/Consulta.js
+++ assets/js/Consulta.js
@@ -1,5 +1,5 @@
 let personajes = (() => {
-  const url = 'https://github.com/latiscool/Torneo-DBZ/blob/main/dbz.json';
+  const url = 'https://raw.githubusercontent.com/latiscool/Torneo-DBZ/main/dbz.json';
   const getData = async () => {
     const res = await fetch(url);
     const data = res.json();
